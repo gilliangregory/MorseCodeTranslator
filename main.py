1f6f7b67159bee504ee7d1cb96239439f3d1ae43
gilliangregory <gillian.gregory@stonybrook.edu>
2022-04-19 15:28:20 -0400

    work on display

--- main.py
+++ main.py
@@ -11,20 +11,43 @@ Select how you want to proceed:
 |2| Encode from English to Morse Code
 |3| Decode from Morse Code to English""")
 
-def display_morsecode(option, rooty, text="", message=""):
+def display_morsecode(option, rooty, morseCode, message=""):
+
+#INPUT OPTION 1 PRINTING   
     if option == "1":
         print("~" * 60)
         drawtree(rooty)
         print("~" * 60)
-
+        
+#INPUT OPTION 2 PRINTING
     if option == "2":
         print("~" * 60)
+        print(morseCode)
         print("~" * 60)
-
+        
+#INPUT OPTION 3 PRINTING
     if option == "3":
         print("~" * 60)
         print("~" * 60)
         
+def process_selection(user_input, option):
+
+#INPUT OPTION 1 PROCESSING
+    if option == "1":
+        return
+
+#INPUT OPTION 2 PROCESSING
+    elif option == "2":
+        text = user_input.upper()
+        morseCode = ""
+
+        for char in text:
+            dotty = []
+            tomorse(rooty, char, dotty)
+            code = "".join(dotty)
+            morseCode = morseCode + code + " "
+            return morseCode
+
 
 def main():
     while True:
@@ -39,6 +62,7 @@ def main():
 
             while True:
 
+#USER SELECTS INPUT OPTION 1
                 if selection in ["1", "tree"]:
                     print("\nDo you want to print the tree? ")
                     user_input = get_input()
@@ -51,6 +75,7 @@ def main():
 
                     display_morsecode("1", rooty)
 
+#USER SELECTS INPUT OPTION 2
                 if selection in ["2", "encode"]:
                     print("\nWhat message do you want to encode from English to Morse Code? ")
                     user_input = get_input()
@@ -61,8 +86,9 @@ def main():
                     elif user_input == "exit":
                         exit()
 
-                    display_morsecode("2",text)
+                    display_morsecode("2",morseCode)
 
+#USER SELECTS INPUT OPTION 3
                 if selection in ["3", "decode"]:
                     print("\nWhat message do you want to decode from Morse Code to English? ")
                     user_input = get_input()
