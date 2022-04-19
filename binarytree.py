4b75c0b959955bb7f50cd700eff7cf2ca7757669
gilliangregory <gillian.gregory@stonybrook.edu>
2022-04-19 11:34:07 -0400

    start tree

---
+++ binarytree.py
@@ -0,0 +1,14 @@
+class Node:
+    def __init__(self,key):
+        self.left = None
+        self.right = None
+        self.val = key
+
+tree = Node("START")
+
+#first level of tree
+tree.left = Node("E")
+tree.right = Node("T")
+
+#second level of tree
+tree.left.left 
