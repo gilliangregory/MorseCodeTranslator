68aa5358c63c814adf1341cd3faa1fca79b2a5fe
gilliangregory <gillian.gregory@stonybrook.edu>
2022-04-19 13:17:51 -0400

    draw tree

--- binarytree.py
+++ binarytree.py
@@ -1,11 +1,10 @@
-#please type "pip install binarytree" into the command line to install
-#binarytree package from python
-from binarytree import Node
 class Node:
     def __init__(self,key):
         self.left = None
         self.right = None
         self.val = key
+    def __repr__(self):
+        return 'TreeNode({})'.format(self.val)
 
 rooty = Node("START")
 
@@ -30,18 +29,18 @@ rooty.right.right.left  = Node("G")
 rooty.right.right.right = Node("O")
 
 #fourth level of tree
-rooty.left.left.left.left    = Node("H")
-rooty.left.left.left.right   = Node("V")
-rooty.left.left.right.left   = Node("F")
-rooty.left.right.left.left   = Node("L")
-rooty.left.right.right.left  = Node("P")
-rooty.left.right.right.right = Node("J")
-rooty.right.left.left.left   = Node("B")
-rooty.right.left.left.right  = Node("X")
-rooty.right.left.right.left  = Node("C")
-rooty.right.left.right.right = Node("Y")
-rooty.right.right.left.left  = Node("Z")
-rooty.right.right.left.right = Node("Q")
+rooty.left.left.left.left     = Node("H")
+rooty.left.left.left.right    = Node("V")
+rooty.left.left.right.left    = Node("F")
+rooty.left.right.left.left    = Node("L")
+rooty.left.right.right.left   = Node("P")
+rooty.left.right.right.right  = Node("J")
+rooty.right.left.left.left    = Node("B")
+rooty.right.left.left.right   = Node("X")
+rooty.right.left.right.left   = Node("C")
+rooty.right.left.right.right  = Node("Y")
+rooty.right.right.left.left   = Node("Z")
+rooty.right.right.left.right  = Node("Q")
 
 def preorder(rooty):
     if rooty:
@@ -55,4 +54,29 @@ def inorder(rooty):
         print(rooty.val),
         inorder(rooty.right)
 
+def drawtree(rooty):
+    def height(rooty):
+        return 1 + max(height(rooty.left), height(rooty.right)) if rooty else -1
+    def jumpto(x, y):
+        t.penup()
+        t.goto(x, y)
+        t.pendown()
+    def draw(node, x, y, dx):
+        if node:
+            t.goto(x, y)
+            jumpto(x, y-20)
+            t.write(node.val, align='center', font=('Arial', 12, 'normal'))
+            draw(node.left, x-dx, y-60, dx/2)
+            jumpto(x, y-20)
+            draw(node.right, x+dx, y-60, dx/2)
+    import turtle
+    t = turtle.Turtle()
+    t.speed(0); turtle.delay(0)
+    h = height(rooty)
+    jumpto(0,20*h)
+    draw(rooty, 0, 30*h, 40*h)
+    t.hideturtle()
+    turtle.mainloop()
 
+if __name__ == '__main__':
+    drawtree(rooty)
